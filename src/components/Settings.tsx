c124005364b51ebbbc1d40a6bf630d974f231a27
linus-jansson <contact@limpan.dev>
2022-11-29 21:51:25 +0100

    removed until later

--- src/components/Settings.tsx
+++ src/components/Settings.tsx
@@ -1,11 +1,5 @@
 export function SettingsMenuHamburgerButton() {
-    return (
-        <div className="w-12 h-12 bg-black rounded-lg flex flex-col items-center justify-center gap-1">
-            <div className="w-6 h-1 bg-white rounded-lg"></div>
-            <div className="w-6 h-1 bg-white rounded-lg"></div>
-            <div className="w-6 h-1 bg-white rounded-lg"></div>
-        </div>
-    )
+    return <></>
 }
 export function SettingsMenu() {
     return (
